3b3241e87477d268d1c634a04b41b332c95ec205
zhihao <khczhihao@gmail.com>
2020-11-16 15:29:29 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    links: [
+        {
+            name: '主頁',
+            value: 'main'
+        },
+        {
+            name: '會員雲',
+            value: 'member'
+        }
+    ]
+}
